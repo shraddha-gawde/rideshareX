947e67361fd7d7318df6543e6287efa6d91beda2
shraddha-gawde <shraddha.gawde1999@gmail.com>
2024-01-24 20:41:53 +0530

    main

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -133,10 +133,10 @@ userRouter.put(
       const userId = req.user.userId;
       const {
         newadminname,
-        newBio,
-        newPhone,
-        newDesignation,
-        newdateofBirth,
+        newbio,
+        newphone,
+        newdesignation,
+        newbirthday,
       } = req.body;
 
       const imageUrl = req.file ? req.file.path : null;
@@ -147,10 +147,10 @@ userRouter.put(
           $set: {
             image: imageUrl.replace(/\\/g, "/"),
             name: newadminname,
-            phone: newPhone,
-            bio: newBio,
-            birthday: newdateofBirth,
-            designation: newDesignation,
+            phone: newphone,
+            bio: newbio,
+            birthday: newbirthday,
+            designation: newdesignation,
           },
         },
         { new: true }
